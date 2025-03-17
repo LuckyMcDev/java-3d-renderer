41522bc8c94bc9f3dd7cb315bfc21a00d06a6122
LuckyMcDev <lucky.dev@myyahoo.com>
2025-03-17 18:58:19 +0100

    add Javadoc and Doxygen

---
+++ javadoc_docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"Alle Klassen und Schnittstellen","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Main.Matrix3"},{"p":"<Unnamed>","l":"SimpleSquareRendering"},{"p":"<Unnamed>","l":"Main.Triangle"},{"p":"<Unnamed>","l":"Main.Vertex"}];updateSearchResults();
